6b1131c52b528eb78e18fdd98066748840e5292f
LiuQ <1639316332@qq.com>
2023-10-26 23:12:20 +0800

    feat: 配置好了knife4j接口文档

---
+++ src/main/java/com/smile67/config/WebMVConfig.java
@@ -0,0 +1,59 @@
+package com.smile67.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author smile67~
+ * @date: 2023/10/26 - 10 - 26 - 23:02
+ * @Description: com.smile67.config
+ * @version: 1.0
+ */
+@Configuration
+@Slf4j
+public class WebMVConfig implements WebMvcConfigurer {
+
+    /**
+     * 通过knife4j生成接口文档
+     *
+     * @return
+     */
+    @Bean
+    public Docket docket() {
+        log.info("准备生成接口文档...");
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("店铺点评")
+                .version("2.0")
+                .description("店铺店铺项目接口文档")
+                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.smile67.controller"))//最重要的一段代码
+                .paths(PathSelectors.any())
+                .build();
+        //接口文档访问地址：http://localhost:8081/doc.html#/home
+        return docket;
+    }
+
+    /**
+     * 设置静态资源映射
+     *
+     * @param registry addResourceHandlers 是重写了父类的方法
+     */
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始设置静态资源映射...");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
