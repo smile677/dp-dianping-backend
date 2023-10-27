899c30a0783ef3b7c1bb43b0bd3d1fead5324222
LiuQ <1639316332@qq.com>
2023-10-27 23:36:04 +0800

    docs: 短信登录-实现登录校验拦截器+完成查询用户信息接口

--- src/main/java/com/smile67/utils/LoginInterceptor.java
+++ src/main/java/com/smile67/utils/LoginInterceptor.java
@@ -4,6 +4,7 @@ import cn.hutool.core.bean.BeanUtil;
 import com.smile67.dto.UserDTO;
 import com.smile67.entity.User;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +18,15 @@ import javax.servlet.http.HttpSession;
  * @version: 1.0
  */
 public class LoginInterceptor implements HandlerInterceptor {
+    /**
+     * 浏览器发送请求 到 DispatcherServlet查询处理器 之后进行拦截
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // return HandlerInterceptor.super.preHandle(request, response, handler);
@@ -35,17 +45,36 @@ public class LoginInterceptor implements HandlerInterceptor {
         BeanUtil.copyProperties(user, userDTO);
         UserHolder.saveUser(userDTO);
         // 5.放行
-        return  true;
+        return true;
     }
 
+    /**
+     * 处理器执行之后，视图渲染之前
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    /**
+     * 视图渲染之后，浏览器收到响应之前
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         // HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
         // 移除ThreadLocal中的用户
         UserHolder.removeUser();
     }
-
-
-
-
 }
