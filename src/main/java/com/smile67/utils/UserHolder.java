c7b9b476485081000694c34a30ec339341cfacf6
LiuQ <1639316332@qq.com>
2023-10-28 15:44:06 +0800

    docs: 规范一些代码注释及命名

--- src/main/java/com/smile67/utils/UserHolder.java
+++ src/main/java/com/smile67/utils/UserHolder.java
@@ -4,17 +4,14 @@ import com.smile67.dto.UserDTO;
 
 public class UserHolder {
     // 泛型使用UserDto隐藏用户敏感信息
-    private static final ThreadLocal<UserDTO> tl = new ThreadLocal<>();
-
+    private static final ThreadLocal<UserDTO> threadLocal = new ThreadLocal<>();
     public static void saveUser(UserDTO user) {
-        tl.set(user);
+        threadLocal.set(user);
     }
-
     public static UserDTO getUser() {
-        return tl.get();
+        return threadLocal.get();
     }
-
     public static void removeUser() {
-        tl.remove();
+        threadLocal.remove();
     }
 }
