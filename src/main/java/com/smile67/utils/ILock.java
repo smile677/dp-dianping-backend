88594904a5363bab747a3cca46f519d6df00035f
LiuQ <1639316332@qq.com>
2023-11-06 23:29:34 +0800

    feat: 分布式锁-Redis分布式锁误删问题

--- src/main/java/com/smile67/utils/ILock.java
+++ src/main/java/com/smile67/utils/ILock.java
@@ -7,7 +7,7 @@ public interface ILock {
     /**
      * 尝试获取锁
      *
-     * @param timeoutSec 锁持有的超时时间，过期后自动释放
+     * @param timeoutSec 锁持有的超时时间，过期后自动释放，作为兜底，防止发生死锁
      * @return ture:代表获取锁成功 false:代表获取锁失败
      */
     boolean tryLock(long timeoutSec);
--- src/main/java/com/smile67/utils/SampleRedisLock.java
+++ src/main/java/com/smile67/utils/SampleRedisLock.java
@@ -1,5 +1,6 @@
 package com.smile67.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
@@ -17,7 +18,7 @@ public class SampleRedisLock implements ILock {
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
-    private String name;
+    private final String name;
 
     public SampleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -25,20 +26,28 @@ public class SampleRedisLock implements ILock {
     }
 
     public static final String KEY_PREFIX = "lock:";
+    public static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     @Override
     public boolean tryLock(long timeoutSec) {
         // 获取线程标识
-        long threadId = Thread.currentThread().getId();
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁
         Boolean success = stringRedisTemplate.opsForValue()
-                .setIfAbsent(KEY_PREFIX + name, threadId + "", timeoutSec, TimeUnit.SECONDS);
+                .setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unlock() {
-        // 释放锁
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        // 获取线程标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取锁中的标识
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        // 判断标识是否一致
+        if (threadId.equals(id)) {
+            // 释放自己的锁
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
